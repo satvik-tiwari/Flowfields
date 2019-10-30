0a928b183a40a5979e67d5dfba3f11bcb24aed59
Kivtas <31216604+satvik-tiwari@users.noreply.github.com>
2019-10-30 17:25:23 +0530

    Update sketch.java

--- Flowfields v1.0/sketch.java
+++ Flowfields v1.0/sketch.java
@@ -2,82 +2,63 @@ class Particle
 {
   PVector pos;
   PVector prevPos;
-  PVector vel;
-  PVector acc;
-  int maxSpeed;
-  int colour;
-  Particle()
- {
-   colour=0;
-   pos = new PVector(random(width),random(height));
-   prevPos = pos.copy();
-   vel = new PVector(0,0);
-   acc = new PVector(0,0);
-   maxSpeed = 4;
- }
-  void update()
+  PVector v;
+int scl = 20;
+int rows, cols;
+float zoff = 0;
+int num_Particles = 13000;
+Particle[] P = new Particle[num_Particles];
+float inc = 0.04;
+PVector[] flowfield;
+
+void setup()
+{
+  size(1000,800);
+  colorMode(HSB, 255);
+   background(0);
+  cols = floor(width/scl);
+  rows = floor(height/scl);
+  for(int i=0; i< P.length; i++)
   {
-   vel.add(acc);
-   vel.limit(maxSpeed); 
-   pos.add(vel);
-   acc.mult(0);
+  P[i] = new Particle();
   }
-  
-  void applyForce(PVector force)
-  {
-   acc.add(force); 
-  }
-  
-  void display()
-  {
-   //stroke(floor(random(0,255)),floor(random(0,255)),floor(random(0,255)),5);
-   stroke(colour, 255,255, 5);
-   colour = colour+1;
-   if(colour>255)
-   colour=0;
-   strokeWeight(1);
-   line(pos.x, pos.y, prevPos.x, prevPos.y);
-   updatePrevPos();
-  }
-  
-  void updatePrevPos()
-  {
-    prevPos.x = pos.x;
-    prevPos.y = pos.y;
-  }
-  
-  void edges()
-  {
-   if(pos.x>width)
-   {
-   pos.x = 0;
-   updatePrevPos();
-   }
-   if(pos.x<0)
-   {
-   pos.x = width;
-   updatePrevPos();
-   }
-   if(pos.y>height)
-   {
-   pos.y = 0;
-   updatePrevPos();
-   }
-   if(pos.y<0)
-   {
-   pos.y = height;
-   updatePrevPos();
-   }
-  }
-  
-  void follow(PVector vectors[])
-  {
-    int x =floor(pos.x/scl);
-    int y= floor(pos.y/scl);
-    int idx = x + y*cols;
-    idx = idx%400;
-   // print(idx+" ");
-    PVector force = vectors[idx];
-    this.applyForce(force);
-  }
+  flowfield = new PVector[cols*rows];
 }
+
+void draw()
+{
+  float xoff = 0;
+  float yoff = 0;
+  
+  for(int y=0; y<rows; y++)
+  {
+    xoff= 0;
+    for(int x=0; x<cols; x++)                   
+    {
+      int idx = x + y * cols;
+      float angle = noise(xoff, yoff, zoff)*TWO_PI*4;
+      v = PVector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[idx] = v;
+      xoff += inc;
+      stroke(0);
+      
+      /*push();
+      translate(x*scl, y*scl);
+      rotate(v.heading());
+      stroke(0,50);
+      strokeWeight(2);
+      line(0, 0, scl, 0);
+      pop();*/
+     }
+    yoff += inc;
+    zoff += 0.00001;
+  }
+  for(int i=0; i<P.length; i++)
+  {
+  P[i].follow(flowfield);  
+  P[i].update();
+  P[i].edges();
+  P[i].display();
+  }
+} 
